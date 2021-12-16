a0c4fbb873c27741bccb17c3aa93c11df30192eb
Scott Meyers <scottmey@gmail.com>
2021-12-16 11:00:00 -0500

    populates table

--- packages/client/src/components/Form.js
+++ packages/client/src/components/Form.js
@@ -1,68 +1,5 @@
-import React, { useCallback, useEffect, useContext, useState } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { SocketContext } from '../context/socket';
-
-const setupTable = () => {
-  // $table.innerHTML = '';
-  // const header = $table.createTHead();
-  // const row = header.insertRow(0);
-  // keys.forEach((key, index) => {
-  //   const column = row.insertCell(index);
-  //   column.innerHTML = key.label;
-  // });
-  // $table.createTBody();
-  // if (!$tableContainer.classList.contains('border-1px')) {
-  //   $tableContainer.classList.add('border-1px');
-  // }
-};
-
-const keys = [
-  // { value: "address1", label: "Address 1" },
-  // { value: "address2", label: "Address 2" },
-  // { value: "agencyId", label: "Agency ID" },
-  { value: 'category', label: 'Category' },
-  // { value: "city", label: "City" },
-  { value: 'comment', label: 'Comment' },
-  // { value: "commentOn", label: "Comment On" },
-  { value: 'commentOnDocumentId', label: 'Comment On Document ID' },
-  { value: 'country', label: 'Country' },
-  // { value: "displayProperties", label: "Display Properties" },
-  // { value: "docAbstract", label: "Doc Abstract" },
-  { value: 'docketId', label: 'Docket ID' },
-  { value: 'documentType', label: 'Document Type' },
-  // { value: "duplicateComments", label: "Duplicate Comments" },
-  { value: 'email', label: 'Email' },
-  // { value: "fax", label: "Fax" },
-  // { value: "field1", label: "Field 1" },
-  // { value: "field2", label: "Field 2" },
-  // { value: "fileFormats", label: "File Formats" },
-  { value: 'firstName', label: 'First Name' },
-  { value: 'govAgency', label: 'Gov Agency' },
-  { value: 'govAgencyType', label: 'Gov Agency Type' },
-  { value: 'lastName', label: 'Last Name' },
-  { value: 'legacyId', label: 'Legacy ID' },
-  // { value: "modifyDate", label: "Modify Date" },
-  // { value: "objectId", label: "Object ID" },
-  // { value: "openForComment", label: "Open For Comment" },
-  { value: 'organization', label: 'Organization' },
-  { value: 'originalDocumentId', label: 'Original Document ID' },
-  // { value: "pageCount", label: "Page Count" },
-  { value: 'phone', label: 'Phone' },
-  { value: 'postedDate', label: 'Posted Date' },
-  // { value: "postmarkDate", label: "Postmark Date" },
-  // { value: "reasonWithdrawn", label: "Reason Withdrawn" },
-  { value: 'receiveDate', label: 'Receive Date' },
-  // { value: "restrictReason", label: "Restrict Reason" },
-  { value: 'restrictReasonType', label: 'Restrict Reason Type' },
-  // { value: "stateProvinceRegion", label: "State Province Region" },
-  // { value: "submitterRep", label: "Submitter Rep" },
-  // { value: "submitterRepAddress", label: "Submitter Rep Address" },
-  // { value: "submitterRepCityState", label: "Submitter Rep City State" },
-  // { value: "subtype", label: "Subtype" },
-  { value: 'title', label: 'Title' },
-  // { value: "trackingNbr", label: "Tracking Number" },
-  // { value: "withdrawn", label: "Withdrawn" },
-  // { value: "zip", label: "Zip" },
-];
 
 const Form = () => {
   const socket = useContext(SocketContext);
@@ -72,19 +9,6 @@ const Form = () => {
   const [documentId, setDocumentId] = useState('CMS-2021-0168-0001');
   const [status, setStatus] = useState('Default');
 
-  const addTableRow = useCallback(
-    (data) => {
-      setComments([...comments, data]);
-      // const $tbody = $table.getElementsByTagName('tbody')[0];
-      // const row = $tbody.insertRow();
-      // keys.forEach((key, index) => {
-      //   const column = row.insertCell(index);
-      //   column.innerHTML = data[key.value];
-      // });
-    },
-    [comments]
-  );
-
   useEffect(() => {
     socket.on('connect', () => {
       setClientId(socket.id);
@@ -92,30 +16,21 @@ const Form = () => {
     });
 
     socket.on('comment', (data) => {
-      addTableRow(data);
+      // overwhelming the browser in this useEffect hook... consider alternatives
+      // setComments([...comments, data]);
+      console.log(data);
     });
 
     socket.on('complete', (data) => {
-      console.log({ complete: data });
-      const { error } = data;
-      if (error) {
-        setStatus(error.message);
-      } else {
-        setStatus('Complete');
-      }
+      setComments(data.comments);
+      setStatus(data.error ? data.error.message : 'Complete');
     });
-  }, [addTableRow, socket]);
+  }, [comments, socket]);
 
-  const handleInput = (e) => {
-    setDocumentId(e.target.value);
-  };
-
-  console.log(comments[0]);
   const handleSubmit = (e) => {
     e.preventDefault();
-
     if (!clientId || !documentId) return null;
-
+    setComments([]);
     fetch(`http://localhost:${process.env.REACT_APP_SERVER_PORT}/comments`, {
       body: JSON.stringify({
         clientId,
@@ -128,9 +43,7 @@ const Form = () => {
     })
       .then((response) => response.json())
       .then((data) => console.log({ data }))
-      .catch((error) => {
-        console.error('Error:', error);
-      });
+      .catch((error) => console.error('Error:', error));
   };
 
   return (
@@ -153,7 +66,7 @@ const Form = () => {
           type="text"
           name="documentId"
           value={documentId}
-          onChange={handleInput}
+          onChange={(e) => setDocumentId(e.target.value)}
         />
         <input
           className="usa-button"
@@ -163,21 +76,117 @@ const Form = () => {
         />
       </form>
 
-      <div
-        className="usa-table-container--scrollable margin-top-3"
-        id="table-container"
-        tabIndex="0"
-      >
-        <table className="usa-table" id="table">
-          {/* {comments.map((comment, i) => {
-            return (
+      {comments?.length ? (
+        <div
+          className="usa-table-container--scrollable margin-top-3"
+          id="table-container"
+          tabIndex="0"
+        >
+          <table className="usa-table" id="table">
+            <thead>
               <tr>
-                <td>{comment?.id}</td>
+                {/* <td>Address 1</td> */}
+                {/* <td>Address 2</td> */}
+                {/* <td>Agency ID</td> */}
+                {/* <td>Category</td> */}
+                {/* <td>City</td> */}
+                <td>Comment</td>
+                {/* <td>Comment On</td> */}
+                <td>Comment On Document ID</td>
+                {/* <td>Country</td> */}
+                {/* <td>Display Properties</td> */}
+                {/* <td>Doc Abstract</td> */}
+                <td>Docket ID</td>
+                <td>Document Type</td>
+                {/* <td>Duplicate Comments</td> */}
+                {/* <td>Email</td> */}
+                {/* <td>Fax</td> */}
+                {/* <td>Field 1</td> */}
+                {/* <td>Field 2</td> */}
+                {/* <td>File Formats</td> */}
+                <td>First Name</td>
+                {/* <td>Gov Agency</td> */}
+                {/* <td>Gov Agency Type</td> */}
+                <td>Last Name</td>
+                {/* <td>Legacy ID</td> */}
+                {/* <td>Modify Date</td> */}
+                {/* <td>Object ID</td> */}
+                {/* <td>Open For Comment</td> */}
+                <td>Organization</td>
+                {/* <td>Original Document ID</td> */}
+                {/* <td>Page Count</td> */}
+                {/* <td>Phone</td> */}
+                <td>Posted Date</td>
+                {/* <td>Postmark Date</td> */}
+                {/* <td>Reason Withdrawn</td> */}
+                <td>Receive Date</td>
+                {/* <td>Restrict Reason</td> */}
+                {/* <td>Restrict Reason Type</td> */}
+                {/* <td>State Province Region</td> */}
+                {/* <td>Submitter Rep</td> */}
+                {/* <td>Submitter Rep Address</td> */}
+                {/* <td>Submitter Rep City State</td> */}
+                {/* <td>Subtype</td> */}
+                <td>Title</td>
+                {/* <td>Tracking Number</td> */}
+                {/* <td>Withdrawn</td> */}
+                {/* <td>Zip</td> */}
               </tr>
-            );
-          })} */}
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {comments.map((comment) => {
+                return (
+                  <tr key={comment.objectId}>
+                    {/* <td>{comment.category}</td> */}
+                    {/* <td>{comment.city}</td> */}
+                    <td>{comment.comment}</td>
+                    {/* <td>{comment.commentOn}</td> */}
+                    <td>{comment.commentOnDocumentId}</td>
+                    {/* <td>{comment.country}</td> */}
+                    {/* <td>{comment.displayProperties}</td> */}
+                    {/* <td>{comment.docAbstract}</td> */}
+                    <td>{comment.docketId}</td>
+                    <td>{comment.documentType}</td>
+                    {/* <td>{comment.duplicateComments}</td> */}
+                    {/* <td>{comment.email}</td> */}
+                    {/* <td>{comment.fax}</td> */}
+                    {/* <td>{comment.field1}</td> */}
+                    {/* <td>{comment.field2}</td> */}
+                    {/* <td>{comment.fileFormats}</td> */}
+                    <td>{comment.firstName}</td>
+                    {/* <td>{comment.govAgency}</td> */}
+                    {/* <td>{comment.govAgencyType}</td> */}
+                    <td>{comment.lastName}</td>
+                    {/* <td>{comment.legacyId}</td> */}
+                    {/* <td>{comment.modifyDate}</td> */}
+                    {/* <td>{comment.objectId}</td> */}
+                    {/* <td>{comment.openForComment}</td> */}
+                    <td>{comment.organization}</td>
+                    {/* <td>{comment.originalDocumentId}</td> */}
+                    {/* <td>{comment.pageCount}</td> */}
+                    {/* <td>{comment.phone}</td> */}
+                    <td>{comment.postedDate}</td>
+                    {/* <td>{comment.postmarkDate}</td> */}
+                    {/* <td>{comment.reasonWithdrawn}</td> */}
+                    <td>{comment.receiveDate}</td>
+                    {/* <td>{comment.restrictReason}</td> */}
+                    {/* <td>{comment.restrictReasonType}</td> */}
+                    {/* <td>{comment.stateProvinceRegion}</td> */}
+                    {/* <td>{comment.submitterRep}</td> */}
+                    {/* <td>{comment.submitterRepAddress}</td> */}
+                    {/* <td>{comment.submitterRepCityState}</td> */}
+                    {/* <td>{comment.subtype}</td> */}
+                    <td>{comment.title}</td>
+                    {/* <td>{comment.trackingNbr}</td> */}
+                    {/* <td>{comment.withdrawn}</td> */}
+                    {/* <td>{comment.zip}</td> */}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      ) : null}
     </div>
   );
 };
