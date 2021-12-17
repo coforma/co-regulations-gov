5fdb0783dd5691ed5c0316f891aa7f1686081dfb
Scott Meyers <scottmey@gmail.com>
2021-12-17 09:59:26 -0500

    outputs to project root

--- packages/server/cli.js
+++ packages/server/cli.js
@@ -3,7 +3,12 @@ require('dotenv').config();
 
 const fs = require('fs');
 const jsonexport = require('jsonexport');
+const path = require('path');
+
 const { getDocumentComments } = require('./services/comments');
+
+const reqPath = path.join(__dirname, '../../');
+const fileName = (type) => `${reqPath}output/${documentId}-output.${type}`;
 
 const documentId = process.argv.slice(2)[0];
 
@@ -26,9 +31,8 @@ const documentId = process.argv.slice(2)[0];
 
     console.log(`There are ${data.comments.length} comments`);
 
-    // write the results to JSON
     fs.writeFile(
-      `output/${documentId}-output.json`,
+      fileName('json'),
       JSON.stringify(data.comments, null, 4),
       'utf8',
       function (err) {
@@ -40,21 +44,15 @@ const documentId = process.argv.slice(2)[0];
       }
     );
 
-    // write the results to CSV
     jsonexport(data.comments, { rowDelimiter: ',' }, function (err, csv) {
       if (err) return console.error(err);
-      fs.writeFile(
-        `output/${documentId}-output.csv`,
-        csv,
-        'utf8',
-        function (err) {
-          if (err) {
-            console.log('An error occured while converting the JSON to CSV.');
-            return console.log(err);
-          }
-          console.log('CSV file has been saved.');
+      fs.writeFile(fileName('csv'), csv, 'utf8', function (err) {
+        if (err) {
+          console.log('An error occured while converting the JSON to CSV.');
+          return console.log(err);
         }
-      );
+        console.log('CSV file has been saved.');
+      });
     });
   } catch (error) {
     throw error;
